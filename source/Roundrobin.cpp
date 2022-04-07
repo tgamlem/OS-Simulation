ddab1fbd00c7b0acffaa56df5736f261c975b195
Alavish <achristopher22@my.whitworth.edu>
2022-04-06 19:34:11 -0700

    round robin changes weds

--- source/Roundrobin.cpp
+++ source/Roundrobin.cpp
@@ -25,27 +25,27 @@ RoundRobin::RoundRobin(int timeSlice) : FCFS() {
     this->timeSlice = timeSlice;
 }
 
-vector<int> RoundRobin::run(string fileName, vector<int> curTime) {
-    for (int i = 0; i < getCPUCount(); i++) {
+int RoundRobin::run(string fileName, int curTime) {
+    
         PCBObject pcb = checkTop();
-        removeReady(curTime[i]);
+        removeReady(curTime);
         if (pcb.getResponseTime() == 0) {
-            pcb.setResponseTime(curTime[i]);
+            pcb.setResponseTime(curTime);
         }
-        pcb.setWaitTime(curTime[i]);
+        pcb.setWaitTime(curTime);
         if (pcb.getExecutionTime() > timeSlice) {
             int partialExecutionTime = pcb.getExecutionTime() - timeSlice;
             pcb.setAccumulatedTime(pcb.getExecutionTime());
             pcb.setExecutionTime(partialExecutionTime);
-            curTime[i] += timeSlice;
+            curTime += timeSlice;
             cout << pcb.getPID() << endl;
-            addReady(pcb, curTime[i]);
+            addReady(pcb, curTime);
         } else {
             pcb.setAccumulatedTime(pcb.getExecutionTime());
-            curTime[i] += pcb.getExecutionTime();
+            curTime += pcb.getExecutionTime();
         }
         csv(fileName, pcb);
-    }
+    
 
     return curTime;
 }
